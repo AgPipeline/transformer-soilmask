52ec573a399e38c21f57bb06c829a6c355c61d5f
Max Burnette <mburnet88@gmail.com>
2016-10-31 11:28:10 -0500

    Add pythonlibs module

--- demosaic/batch_launcher.sh
+++ demosaic/batch_launcher.sh
@@ -3,7 +3,7 @@
 
 # Load necessary modules
 module purge
-module load python/2.7.10 gdal gdal-stack-2.7.10 netcdf
+module load python/2.7.10 pythonlibs/2.7.10 gdal gdal-stack-2.7.10 netcdf
 
 # Activate python virtualenv
 source /home/mburnet2/extractors/pyenv/bin/activate
