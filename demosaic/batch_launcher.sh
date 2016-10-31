dc34caed45b08cb47c81faa52ede3ab8d4d72a33
Max Burnette <mburnet88@gmail.com>
2016-10-31 10:25:44 -0500

    First draft of batch launcher

---
+++ demosaic/batch_launcher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this script is called to invoke one instance of demosaic extractor.
+
+# Load necessary modules
+module purge
+module load python/2.7.10 gdal-stack-2.7.10 netcdf
+
+# Activate python virtualenv
+source /home/mburnet2/extractors/pyenv/bin/activate
+
+# Run extractor script
+python /home/mburnet2/extractors/extractors-stereo-rgb/demosaic/terra.demosaic.py
