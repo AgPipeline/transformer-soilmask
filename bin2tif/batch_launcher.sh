47b781becfe779ebe95d971bff3e6004602e057e
Max Burnette <mburnet88@gmail.com>
2017-04-27 10:28:22 -0500

    Update extractor names.

--- bin2tif/batch_launcher.sh
+++ bin2tif/batch_launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# this script is called to invoke one instance of demosaic extractor.
+# this script is called to invoke one instance of bin2tif extractor.
 
 # Load necessary modules
 module purge
@@ -9,4 +9,4 @@ module load python/2.7.10 pythonlibs/2.7.10 gdal-stack-2.7.10 gdal netcdf
 source /projects/arpae/terraref/shared/extractors/pyenv/bin/activate
 
 # Run extractor script
-python /projects/arpae/terraref/shared/extractors/extractors-stereo-rgb/demosaic/terra_demosaic.py
+python /projects/arpae/terraref/shared/extractors/extractors-stereo-rgb/bin2tif/terra_bin2tif.py
