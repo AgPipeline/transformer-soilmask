d61894be82c3e2e980bd66265d859064b95abf26
Max Burnette <mburnet88@gmail.com>
2017-04-28 08:44:34 -0500

    Update rule logic for quick rejection

--- fieldmosaic/rules.py
+++ fieldmosaic/rules.py
@@ -15,49 +15,57 @@ logging.getLogger('__main__').setLevel(logging.DEBUG)
 # https://opensource.ncsa.illinois.edu/bitbucket/projects/CATS/repos/extractors-rulechecker
 def fullFieldMosaicStitcher(extractor, connector, host, secret_key, resource, rulemap):
     results = {}
+    full_field_ready = False
 
     # Determine output dataset
     dsname = resource["dataset_info"]["name"]
     sensor = dsname.split(" - ")[0]
-    timestamp = dsname.split(" - ")[1]
-    date = timestamp.split("__")[0]
-    progress_key = "Full Field - " + sensor + " - " + date
 
-    # Fetch all existing file IDs that would be fed into this field mosaic
-    progress = rule_utils.retrieveProgressFromDB(progress_key)
-    progress['ids'] += resource['ids']
-    full_field_ready = False
+    if sensor == "stereoTop":
+        timestamp = dsname.split(" - ")[1]
+        date = timestamp.split("__")[0]
+        progress_key = "Full Field - " + sensor + " - " + date
 
-    # Check to see if list of geotiffs is same length as list of raw datasets
-    date_directory = "/home/extractor/sites/ua-mac/raw_data/stereoTop/%s" % date
-    logging.debug("counting raw files in %s..." % date_directory)
-    raw_file_count = int(subprocess.check_output("find %s -maxdepth 1 | wc -l" % date_directory,
-                                             shell=True).strip())
-    logging.debug("found %s raw files" % raw_file_count)
+        # Fetch all existing file IDs that would be fed into this field mosaic
+        progress = rule_utils.retrieveProgressFromDB(progress_key)
+        progress['ids'] += resource['ids']
 
-    # If we have all raw files accounted for and more than 6000 (typical daily magnitude) listed, trigger
-    if len(progress['ids']) == raw_file_count and len(progress['ids']) > 6000:
-        full_field_ready = True
-    else:
-        logging.debug("only found %s/%s necessary geotiffs" % (len(progress['ids']), raw_file_count))
+        # Check to see if list of geotiffs is same length as list of raw datasets
+        date_directory = "/home/extractor/sites/ua-mac/raw_data/stereoTop/%s" % date
+        logging.debug("counting raw files in %s..." % date_directory)
+        raw_file_count = int(subprocess.check_output("find %s -maxdepth 1 | wc -l" % date_directory,
+                                                 shell=True).strip())
+        logging.debug("found %s raw files" % raw_file_count)
 
-    if full_field_ready:
-        for extractor in rulemap["extractors"]:
-            results[extractor] = {
-                "process": True,
-                "parameters": {
-                    "file_ids": progress["ids"],
-                    "output_dataset": "Full Field - "+date
+        # If we have all raw files accounted for and more than 6000 (typical daily magnitude) listed, trigger
+        if len(progress['ids']) == raw_file_count and len(progress['ids']) > 6000:
+            full_field_ready = True
+        else:
+            logging.debug("only found %s/%s necessary geotiffs" % (len(progress['ids']), raw_file_count))
+
+        if full_field_ready:
+            for extractor in rulemap["extractors"]:
+                results[extractor] = {
+                    "process": True,
+                    "parameters": {
+                        "file_ids": progress["ids"],
+                        "output_dataset": "Full Field - "+date
+                    }
                 }
-            }
+        else:
+            for extractor in rulemap["extractors"]:
+                results[extractor] = {
+                    "process": False,
+                    "parameters": {
+                        "file_ids": progress["ids"]
+                    }
+                }
+                rule_utils.submitProgressToDB("fullFieldMosaicStitcher", extractor, progress_key, progress["ids"])
     else:
         for extractor in rulemap["extractors"]:
             results[extractor] = {
                 "process": False,
-                "parameters": {
-                    "file_ids": progress["ids"]
-                }
+                "parameters": {}
             }
-            rule_utils.submitProgressToDB("fullFieldMosaicStitcher", extractor, progress_key, progress["ids"])
 
     return results
