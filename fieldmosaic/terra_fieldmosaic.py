c591c3c638b6726f84986d29cbed8d146db25d6a
Max Burnette <mburnet88@gmail.com>
2017-07-17 11:07:17 -0500

    Add shade removal module

--- fieldmosaic/terra_fieldmosaic.py
+++ fieldmosaic/terra_fieldmosaic.py
@@ -16,6 +16,7 @@ import pyclowder.datasets
 import terrautils.extractors
 
 import full_day_to_tiles
+import shadeRemoval as shade
 
 
 class FullFieldMosaicStitcher(Extractor):
@@ -36,6 +37,10 @@ class FullFieldMosaicStitcher(Extractor):
                                  help="whether to overwrite output file if it already exists in output directory")
         self.parser.add_argument('--mainspace', dest="mainspace", type=str, nargs='?',
                                  default="58da6b924f0c430e2baa823f", help="Space UUID in Clowder to store results")
+        self.parser.add_argument('--darker', dest="generate_darker", type=bool, nargs='?', default=False,
+                                 help="whether to use multipass mosiacking to select darker pixels")
+        self.parser.add_argument('--split', dest="split_num", type=int, nargs='?', default=2,
+                                 help="number of splits to use if --darker is True")
         self.parser.add_argument('--influxHost', dest="influx_host", type=str, nargs='?',
                                  default=influx_host, help="InfluxDB URL for logging")
         self.parser.add_argument('--influxPort', dest="influx_port", type=int, nargs='?',
@@ -58,6 +63,8 @@ class FullFieldMosaicStitcher(Extractor):
         self.output_dir = self.args.output_dir
         self.force_overwrite = self.args.force_overwrite
         self.mainspace = self.args.mainspace
+        self.generate_darker = self.args.generate_darker
+        self.split_num = self.args.split_num
         self.influx_params = {
             "host": self.args.influx_host,
             "port": self.args.influx_port,
@@ -80,6 +87,71 @@ class FullFieldMosaicStitcher(Extractor):
         out_vrt = os.path.join(out_dir, out_root+".vrt")
         out_tif_full = os.path.join(out_dir, out_root+".tif")
         out_tif_thumb = os.path.join(out_dir, out_root+"_thumb.tif")
+
+        nu_created, nu_bytes = 0, 0
+        if not self.generate_darker:
+            (nu_created, nu_bytes) = self.generateSingleMosaic(out_dir, out_vrt, out_tif_thumb, out_tif_full, parameters)
+        else:
+            (nu_created, nu_bytes) = self.generateDarkerMosaic(out_dir, out_vrt, out_tif_thumb, out_tif_full, parameters)
+        created += nu_created
+        bytes += nu_bytes
+
+        # Upload full field image to Clowder
+        parent_collect = self.getCollectionOrCreate(connector, host, secret_key, "Full Field Stitched Mosaics",
+                                                    parent_space=self.mainspace)
+        year_collect = self.getCollectionOrCreate(connector, host, secret_key, parameters["output_dataset"][:17],
+                                                  parent_collect, self.mainspace)
+        month_collect = self.getCollectionOrCreate(connector, host, secret_key, parameters["output_dataset"][:20],
+                                                   year_collect, self.mainspace)
+        target_dsid = self.getDatasetOrCreate(connector, host, secret_key, parameters["output_dataset"],
+                                              month_collect, self.mainspace)
+
+        thumbid = pyclowder.files.upload_to_dataset(connector, host, secret_key, target_dsid, out_tif_thumb)
+        fullid = pyclowder.files.upload_to_dataset(connector, host, secret_key, target_dsid, out_tif_full)
+
+        content = {
+            "comment": "This stitched image is computed based on an assumption that the scene is planar. \
+                There are likely to be be small offsets near the boundary of two images anytime there are plants \
+                at the boundary (because those plants are higher than the ground plane), or where the dirt is \
+                slightly higher or lower than average.",
+            "file_ids": parameters["file_ids"]
+        }
+        thumbmeta = terrautils.extractors.build_metadata(host, self.extractor_info['name'], thumbid, content, 'file')
+        pyclowder.files.upload_metadata(connector, host, secret_key, thumbid, thumbmeta)
+        fullmeta = terrautils.extractors.build_metadata(host, self.extractor_info['name'], fullid, content, 'file')
+        pyclowder.files.upload_metadata(connector, host, secret_key, thumbid, fullmeta)
+
+        endtime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        terrautils.extractors.log_to_influxdb(self.extractor_info['name'], self.influx_params,
+                                              starttime, endtime, created, bytes)
+
+    def getCollectionOrCreate(self, connector, host, secret_key, cname, parent_colln=None, parent_space=None):
+        # Fetch dataset from Clowder by name, or create it if not found
+        url = "%sapi/collections?key=%s&title=" % (host, secret_key, cname)
+        result = requests.get(url, verify=connector.ssl_verify)
+        result.raise_for_status()
+
+        if len(result.json()) == 0:
+            return pyclowder.collections.create_empty(connector, host, secret_key, cname, "",
+                                                      parent_colln, parent_space)
+        else:
+            return result.json()[0]['id']
+
+    def getDatasetOrCreate(self, connector, host, secret_key, dsname, parent_colln=None, parent_space=None):
+        # Fetch dataset from Clowder by name, or create it if not found
+        url = "%sapi/datasets?key=%s&title=" % (host, secret_key, dsname)
+        result = requests.get(url, verify=connector.ssl_verify)
+        result.raise_for_status()
+
+        if len(result.json()) == 0:
+            return pyclowder.datasets.create_empty(connector, host, secret_key, dsname, "",
+                                                   parent_colln, parent_space)
+        else:
+            return result.json()[0]['id']
+
+    def generateSingleMosaic(self, out_dir, out_vrt, out_tif_thumb, out_tif_full, parameters):
+        # Create simple mosaic from geotiff list
+        created, bytes = 0, 0
 
         if (not os.path.isfile(out_vrt)) or self.force_overwrite:
             logging.info("processing %s TIFs" % len(parameters['file_ids']))
@@ -111,60 +183,55 @@ class FullFieldMosaicStitcher(Extractor):
             created += 1
             bytes += os.path.getsize(out_tif_full)
 
+        return (created, bytes)
 
-        # Upload full field image to Clowder
-        parent_collect = self.getCollectionOrCreate(connector, host, secret_key, "Full Field Stitched Mosaics",
-                                                    parent_space=self.mainspace)
-        year_collect = self.getCollectionOrCreate(connector, host, secret_key, parameters["output_dataset"][:17],
-                                                  parent_collect, self.mainspace)
-        month_collect = self.getCollectionOrCreate(connector, host, secret_key, parameters["output_dataset"][:20],
-                                                   year_collect, self.mainspace)
-        target_dsid = self.getDatasetOrCreate(connector, host, secret_key, parameters["output_dataset"],
-                                              month_collect, self.mainspace)
+    def generateDarkerMosaic(self, out_dir, out_vrt, out_tif_thumb, out_tif_full, parameters):
+        # Create dark-pixel mosaic from geotiff list using multipass for darker pixel selection
+        created, bytes = 0, 0
 
-        thumbid = pyclowder.files.upload_to_dataset(connector, host, secret_key, target_dsid, out_tif_thumb)
-        fullid = pyclowder.files.upload_to_dataset(connector, host, secret_key, target_dsid, out_tif_full)
+        if (not os.path.isfile(out_vrt)) or self.force_overwrite:
+            # Write input list to tmp file
+            with open("tiflist.txt", "w") as tifftxt:
+                for t in parameters["file_ids"]:
+                    tifftxt.write("%s/n" % t)
 
+            # Split full tiflist into parts according to split number
+            shade.split_tif_list("tiflist.txt", out_dir, self.split_num)
+            os.remove("tiflist.txt")
 
-        content = {
-            "comment": "This stitched image is computed based on an assumption that the scene is planar. \
-                There are likely to be be small offsets near the boundary of two images anytime there are plants \
-                at the boundary (because those plants are higher than the ground plane), or where the dirt is \
-                slightly higher or lower than average.",
-            "file_ids": parameters["file_ids"]
-        }
-        thumbmeta = terrautils.extractors.build_metadata(host, self.extractor_info['name'], thumbid, content, 'file')
-        pyclowder.files.upload_metadata(connector, host, secret_key, thumbid, thumbmeta)
-        fullmeta = terrautils.extractors.build_metadata(host, self.extractor_info['name'], fullid, content, 'file')
-        pyclowder.files.upload_metadata(connector, host, secret_key, thumbid, fullmeta)
+            # Generate tiles from each split VRT into numbered folders
+            shade.create_diff_tiles_set(out_dir, self.split_num)
 
-        endtime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-        terrautils.extractors.log_to_influxdb(self.extractor_info['name'], self.influx_params,
-                                              starttime, endtime, created, bytes)
+            # Choose darkest pixel from each overlapping tile
+            unite_tiles_dir = os.path.join(out_dir, 'unite')
+            shade.integrate_tiles(out_dir, unite_tiles_dir, self.split_num)
 
-    # Fetch dataset from Clowder by name, or create it if not found
-    def getCollectionOrCreate(self, connector, host, secret_key, cname, parent_colln=None, parent_space=None):
-        url = "%sapi/collections?key=%s&title=" % (host, secret_key, cname)
-        result = requests.get(url, verify=connector.ssl_verify)
-        result.raise_for_status()
+            # If any files didn't have overlap, copy individual tile
+            shade.copy_missing_tiles(out_dir, unite_tiles_dir, self.split_num, tiles_folder_name='tiles_left')
 
-        if len(result.json()) == 0:
-            return pyclowder.collections.create_empty(connector, host, secret_key, cname, "",
-                                                      parent_colln, parent_space)
-        else:
-            return result.json()[0]['id']
+            # Create output VRT from overlapped tiles
+            # TODO: Adjust this step so google HTML isn't generated?
+            shade.create_unite_tiles(unite_tiles_dir, out_vrt)
+            created += 1
+            bytes += os.path.getsize(out_vrt)
 
-    # Fetch dataset from Clowder by name, or create it if not found
-    def getDatasetOrCreate(self, connector, host, secret_key, dsname, parent_colln=None, parent_space=None):
-        url = "%sapi/datasets?key=%s&title=" % (host, secret_key, dsname)
-        result = requests.get(url, verify=connector.ssl_verify)
-        result.raise_for_status()
+        if (not os.path.isfile(out_tif_thumb)) or self.force_overwrite:
+            # Convert VRT to full-field GeoTIFF (low-res then high-res)
+            logging.info("Converting VRT to %s..." % out_tif_thumb)
+            subprocess.call(["gdal_translate -projwin -111.9750277 33.0764277 -111.9748097 33.0745861 "+
+                             "-outsize 10% 10% %s %s" % (out_vrt, out_tif_thumb)])
+            created += 1
+            bytes += os.path.getsize(out_tif_thumb)
 
-        if len(result.json()) == 0:
-            return pyclowder.datasets.create_empty(connector, host, secret_key, dsname, "",
-                                                   parent_colln, parent_space)
-        else:
-            return result.json()[0]['id']
+        if (not os.path.isfile(out_tif_full)) or self.force_overwrite:
+            logging.info("Converting VRT to %s..." % out_tif_full)
+            subprocess.call(["gdal_translate -projwin -111.9750277 33.0764277 -111.9748097 33.0745861 "+
+                             "%s %s" % (out_vrt, out_tif_full)])
+            created += 1
+            bytes += os.path.getsize(out_tif_full)
+
+        return (created, bytes)
+
 
 if __name__ == "__main__":
     extractor = FullFieldMosaicStitcher()
